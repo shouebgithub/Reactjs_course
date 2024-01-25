a07ddd1227b5d3a3cd3006757a1bee21c02c2a75
shouebahmed <shouebahmed@digital-nirvana.com>
2024-01-25 13:41:14 +0530

    video 6 prop & prop types

---
+++ module1.mjs
@@ -0,0 +1,6 @@
+import d, {a, b, c} from './module2.mjs';
+
+console.log(d);
+console.log(b);
+console.log(c);
+console.log(a);
---
+++ module2.mjs
@@ -0,0 +1,10 @@
+const a = "Anas";
+const b = "Bashra";
+const c = "Chand";
+const d = "Dawood";
+
+
+export default d;
+export {a};
+export {b};
+export {c};
